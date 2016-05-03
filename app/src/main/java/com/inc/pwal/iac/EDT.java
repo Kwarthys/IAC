5c2d8ca580a1bd91dd7c1d1248af5e0f30d2b935
Kwarthys <kwarthys@gmail.com>
2016-05-03 12:25:46 +0200

    pwal

---
+++ app/src/main/java/com/inc/pwal/iac/EDT.java
@@ -0,0 +1,22 @@
+package com.inc.pwal.iac;
+
+import android.util.Log;
+import android.util.Property;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * Created by Kwarthys on 03/05/2016.
+ */
+public class EDT
+{
+
+}
