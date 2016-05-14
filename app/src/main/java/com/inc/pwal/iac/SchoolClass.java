4c2afeac785b09faceaa8619294d0fedbf9adcb1
Kwarthys <kwarthys@gmail.com>
2016-05-14 21:19:30 +0200

    Make EDT completement fonctionel

--- app/src/main/java/com/inc/pwal/iac/SchoolClass.java
+++ app/src/main/java/com/inc/pwal/iac/SchoolClass.java
@@ -1,5 +1,7 @@
 package com.inc.pwal.iac;
 
+import java.util.Date;
+
 /**
  * Created by Kwarthys on 03/05/2016.
  */
@@ -9,6 +11,43 @@ public class SchoolClass {
     private String subject;
     private String teacher;
     private String room;
+    private Date date;
+    private boolean mark;
+
+    public SchoolClass()
+    {
+        date = null; teacher = null; subject = null; room = null;
+    }
+
+    public SchoolClass(SchoolClass c) {
+        // TODO Auto-generated constructor stub
+        date = c.date;
+        teacher = c.teacher;
+        subject = c.subject;
+        room = c.room;
+    }
+
+    public boolean isValid() {
+        // TODO Auto-generated method stub
+            return ( (date!=null) && (teacher!=null) && (subject!=null) && (room!=null));
+    }
+
+    public void reset()
+    {
+        // TODO Auto-generated method stub
+        date = null; teacher = null; subject = null; room = null;
+    }
+
+
+    //--------------------GETTERS & SETTERS------------------------------
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
     public String getSubject() {
         return subject;
@@ -33,4 +72,9 @@ public class SchoolClass {
     public void setRoom(String room) {
         this.room = room;
     }
+
+    public void setMark(boolean m){mark = m;}
+
+    public boolean isMarked(){return mark;}
+
 }
