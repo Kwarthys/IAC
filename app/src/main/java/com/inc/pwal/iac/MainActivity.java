9acaf98aaf4a25acf2de29a2edcef55d74d27784
PierreMahieux <pierremahieux@hotmail.fr>
2016-05-17 22:14:15 +0200

    17/05/2016 TODO onclickListener passage paramètre bouton clické

--- app/src/main/java/com/inc/pwal/iac/MainActivity.java
+++ app/src/main/java/com/inc/pwal/iac/MainActivity.java
@@ -1,5 +1,6 @@
 package com.inc.pwal.iac;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,30 +18,36 @@ import java.util.ArrayList;
 
 //pwal
 
-public class MainActivity extends AppCompatActivity {   //activité du menu principal (affichage des horaires de chaque jour)
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{   //activité du menu principal (affichage des horaires de chaque jour)
 
 
-    private Day day1;
+    private Day defaultDay;
+    private Day monday;
     private Week week1;
     private Alarm alarm;
-    private Rituel rit1;
+    private Rituel douche;
+    private final String DAY_CLICKED="DayPassed";
 
     @Override
    protected void onCreate(Bundle savedInstanceState)
     {
-        rit1 = new Rituel(1,"douche",25,0,"pwal");
+        ArrayList<Rituel> rituelsEmpty=new ArrayList<>();
+        defaultDay=new Day(rituelsEmpty,42,"DEFAULT",new Hour(24,0));
+
+        douche = new Rituel(1,"douche",25,0,"pwal");
         ArrayList<Rituel> rituels = new ArrayList<Rituel>();
-        rituels.add(rit1);
-        day1=new Day(rituels,1,"Lundi",new Hour(12,0));
+        rituels.add(douche);
+        monday=new Day(rituels,1,"Lundi",new Hour(12,0));
         super.onCreate(savedInstanceState);
 
         int view = R.layout.activity_main;
         setContentView(view);
 
         Button buttonMonday = (Button) findViewById(R.id.buttonMonday);
-        int H = day1.getAlarmHour().getHours();
-        int M = day1.getAlarmHour().getMinutes();
-        buttonMonday.setText("Lundi "+H+":"+M);
+        int H = monday.getAlarmHour().getHours();
+        int M = monday.getAlarmHour().getMinutes();
+        buttonMonday.setText(monday.getName()+" " + H + ":" + M);
+        buttonMonday.setOnClickListener(MainActivity.this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -66,5 +73,12 @@ public class MainActivity extends AppCompatActivity {   //activité du menu prin
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        /*Intent intent = new Intent(MainActivity.this, DaySettingsActivity.class);
+        intent.putExtra(DAY_CLICKED,v);
+        startActivity(intent);*/
     }
 }
